51f8426135c19286f5eb2704fe0f34a0ff763f56
Danny Díaz <danny.diaz@epn.edu.ec>
2020-06-29 09:23:02 -0500

    Configuración de entorno

---
+++ 02-Android/app/src/main/java/com/diaz_danny/DeusGallet/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.diaz_danny.DeusGallet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
