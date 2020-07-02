6b239a8026e70f45ebc4415f9917f0cc42b19973
Danny Díaz <danny.diaz@epn.edu.ec>
2020-07-02 10:43:55 -0500

    Logs en el ciclo de vida de la aplicación

--- 02-Android/app/src/main/java/com/diaz_danny/DeusGallet/MainActivity.kt
+++ 02-Android/app/src/main/java/com/diaz_danny/DeusGallet/MainActivity.kt
@@ -2,11 +2,47 @@ package com.diaz_danny.DeusGallet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG = "MainActivity";
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.i(TAG, "OnCreate")
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "OnStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(TAG, "onRestart")
+    }
+
 }
